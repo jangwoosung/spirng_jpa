45f4570a12eb023f18378b346a75fc380c11b6c7
ws7307.jang <ws7307.jang@partner.samsung.com>
2022-09-19 13:16:12 +0900

    * Jpa - 주문,주문상품 엔티티 개발2 - 조건 검색은 다른강좌에서

---
+++ src/main/java/jpabook/springjpa/repository/orderRepository.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * created by        : jws
+ * creation date     : 2022-09-19
+ *
+ * Copyright (c) 2021 Samsung SDS.
+ * All rights reserved.
+ *******************************************************************************/
+
+package jpabook.springjpa.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+
+import jpabook.springjpa.domain.Order;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Class description
+ *
+ * @author jws
+ * @since 2022. 9. 19
+ * @version 1.0
+*/
+
+@Repository
+@RequiredArgsConstructor
+public class orderRepository {
+
+	private final EntityManager em;
+
+	public void save(Order order) {
+		em.persist(order);
+	}
+
+	public Order findOne(Long id) {
+		return em.find(Order.class, id);
+	}
+
+	//public List<Order> findAll(OrderSearch orderSearch) {}
+}
