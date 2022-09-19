2f720cc5b954ad0727a2790d8a9c0fe6a49f2323
ws7307.jang <ws7307.jang@partner.samsung.com>
2022-09-19 12:13:32 +0900

    * Jpa 9일차 - 상품 엔티티 개발3

---
+++ src/main/java/jpabook/springjpa/service/ItemService.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * created by        : jws
+ * creation date     : 2022-09-19
+ *
+ * Copyright (c) 2021 Samsung SDS.
+ * All rights reserved.
+ *******************************************************************************/
+
+package jpabook.springjpa.service;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.springjpa.domain.item.Item;
+import jpabook.springjpa.repository.ItempRepository;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Class description
+ *
+ * @author jws
+ * @since 2022. 9. 19
+ * @version 1.0
+*/
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+	private final ItempRepository itempRepository;
+
+	@Transactional
+	public void saveItem(Item item) {
+		itempRepository.save(item);
+	}
+
+	public List<Item> findItems() {
+		return itempRepository.findAll();
+	}
+
+	public Item findOne(Long itemId) {
+		return itempRepository.findOne(itemId);
+	}
+}
