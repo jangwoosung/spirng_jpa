98ea919062b75e05a6c670ec0d0925aff7aa80d7
ws7307.jang <ws7307.jang@partner.samsung.com>
2022-10-20 15:50:25 +0900

    * Jpa - 웹계층개발6

--- src/main/java/jpabook/springjpa/repository/ItemRepository.java
+++ src/main/java/jpabook/springjpa/repository/ItemRepository.java
@@ -44,6 +44,6 @@ public class ItemRepository {
 	}
 
 	public List<Item> findAll() {
-		return em.createQuery("select i form Item i", Item.class).getResultList();
+		return em.createQuery("select i from Item i", Item.class).getResultList();
 	}
 }
